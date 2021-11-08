f910546cca47630b058e8d556089421ed65502c7
wluyima <wyclif@openmrs.org>
2021-11-08 11:13:06 -0600

    EC-84 - Switched to using MySQL for the mgt database in the dbsync apps, added patient matching configuration

--- dbsync-central/mgt_db_config.sql
+++ dbsync-central/mgt_db_config.sql
@@ -1,1 +1,13 @@
+USE openmrs_eip_mgt;
+
+CREATE TABLE `site_info` (
+     `id` bigint(20) NOT NULL AUTO_INCREMENT,
+     `name` varchar(255) NOT NULL,
+     `identifier` varchar(255) NOT NULL,
+     `date_created` datetime NOT NULL,
+     PRIMARY KEY (`id`),
+     UNIQUE KEY `site_info_identifier_uk` (`identifier`),
+     UNIQUE KEY `site_info_name_uk` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 INSERT INTO site_info (name, identifier, date_created) VALUES ('Remote', 'Remote', now());
