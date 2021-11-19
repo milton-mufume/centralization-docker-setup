acb827be422001a7bec704e0509ea6a9e926b0c8
wluyima <wyclif@openmrs.org>
2021-11-19 13:15:02 -0600

    EC-98 - Fixed OpenMRS download URL and command in the openmrs containers

--- openmrs-common/run.sh
+++ openmrs-common/run.sh
@@ -4,7 +4,7 @@ apt-get update
 apt-get install -y curl
 
 if test ! -f "/usr/local/tomcat/webapps/openmrs.war"; then
-  curl https://versaweb.dl.sourceforge.net/project/openmrs/releases/OpenMRS_Platform_2.3.3/openmrs.war -o /usr/local/tomcat/webapps/openmrs.war
+  curl -L https://downloads.sourceforge.net/project/openmrs/releases/OpenMRS_Platform_2.3.3/openmrs.war -o /usr/local/tomcat/webapps/openmrs.war
 fi
 
 ./bin/catalina.sh run
