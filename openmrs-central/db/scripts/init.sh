54d6090854e8517afc98b70c5063d5521a9d55de
JP Boane <jpboane@gmail.com>
2022-01-18 11:18:27 +0200

    Added the conf directory with conf template files for all services

--- openmrs-central/db/scripts/init.sh
+++ openmrs-central/db/scripts/init.sh
@@ -3,7 +3,11 @@
 apt-get update
 apt-get install -y cron
 apt-get install -y vim
+apt-get install -y procps
+
+env > /home/env.sh
 
 /home/scripts/install_crons.sh
+service cron start
 
 exec /usr/sbin/mysqld --user=root --console
