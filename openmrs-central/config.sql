cd4b0f4c113cd27077791af2659e4c2fb19cae43
wluyima <wyclif@openmrs.org>
2022-03-18 16:47:58 -0500

    EC-153 - Use different system URIs for each identifier type

--- openmrs-central/config.sql
+++ openmrs-central/config.sql
@@ -10,12 +10,13 @@ VALUES ('debezium.engine.enabled', 'true', 'ace8f66d-98b8-4829-a5af-d063376cd5c1
        ('mpi.keystore.type', 'PKCS12', '4be8f66d-98b8-4829-a5af-d063376cd5c1'),
        ('mpi.relationship.type.personA.concept.mappings', '8d91a210-c2cc-11de-8d13-0010c6dffd0f:BP:Biological Patient,8d919b58-c2cc-11de-8d13-0010c6dffd0f:PD:Personal Doctor', '5be8f66d-98b8-4829-a5af-d063376cd5c1'),
        ('mpi.relationship.type.personB.concept.mappings', '8d91a210-c2cc-11de-8d13-0010c6dffd0f:BC:Biological Child,8d919b58-c2cc-11de-8d13-0010c6dffd0f:P:Patient', '6be8f66d-98b8-4829-a5af-d063376cd5c1'),
-       ('mpi.openmrs.uuid.concept.map', 'OpenMRS_UUID:OpenMRS UUID', '7be8f66d-98b8-4829-a5af-d063376cd5c1'),
+       ('mpi.patient.uuid.concept.map', 'OpenMRS_UUID:OpenMRS UUID', '7be8f66d-98b8-4829-a5af-d063376cd5c1'),
        ('mpi.patient.health.center.url', 'http://openmrs.org/fhir/StructureDefinition/patient-healthCenter', '8be8f66d-98b8-4829-a5af-d063376cd5c1'),
        ('mpi.person.uuid.url', 'http://openmrs.org/fhir/StructureDefinition/person-uuid', '9be8f66d-98b8-4829-a5af-d063376cd5c1'),
        ('mpi.relationship.type.terminology.system.uri', 'http://relationship.type.concept', '0ce8f66d-98b8-4829-a5af-d063376cd5c1'),
        ('mpi.identifier.type.terminology.system.uri', 'http://identifier.type.concept', '1ce8f66d-98b8-4829-a5af-d063376cd5c1'),
        ('mpi.identifier.type.concept.mappings', '15a3fd64-1d5f-11e0-b929-000c29ad1d08:ND:The NID', '2ce8f66d-98b8-4829-a5af-d063376cd5c1'),
-       ('mpi.identifier.system.uri', 'http://test.id.com', '3ce8f66d-98b8-4829-a5af-d063376cd5c1');
+       ('mpi.patient.uuid.system.uri', 'http://test.id.com/uuid', '3ce8f66d-98b8-4829-a5af-d063376cd5c1'),
+       ('mpi.identifier.type.system.uri.mappings', '15a3fd64-1d5f-11e0-b929-000c29ad1d08^http://test.id.com/nid,8d79403a-c2cc-11de-8d13-0010c6dffd0f^http://test.id.com/openmrs-old-id', '4ce8f66d-98b8-4829-a5af-d063376cd5c1');
 
 UPDATE global_property SET property_value = 'org.openmrs.api:info,org.openmrs.module.debezium:debug,org.openmrs.module.fgh.mpi:debug,io.debezium:info' WHERE property = 'log.level';
