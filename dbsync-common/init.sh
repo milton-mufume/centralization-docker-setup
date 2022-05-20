59b22e22386892e5336a55bf87b9ad37f813db8d
wluyima <wyclif@openmrs.org>
2022-05-20 18:53:26 -0500

    EC-93 - Disable complex obs sync routes both in the sender and receiver

--- dbsync-common/init.sh
+++ dbsync-common/init.sh
@@ -10,6 +10,6 @@ if test ! -f "openmrs-eip-app.jar"; then
   git clone https://github.com/FriendsInGlobalHealth/openmrs-eip.git
   cd openmrs-eip
   mvn clean install -DskipTests
-  cp app/target/openmrs-eip-app-1.0-SNAPSHOT.jar ../openmrs-eip-app.jar
+  cp app/target/openmrs-eip-app-1.1-SNAPSHOT.jar ../openmrs-eip-app.jar
   cd ..
 fi
