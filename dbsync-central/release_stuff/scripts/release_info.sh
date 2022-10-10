f06f7ec4121116b7924cc5f85ee150310e9a9377
JPboane <jpboane@gmail.com>
2022-10-10 11:55:19 +0200

    Updated release info

--- dbsync-central/release_stuff/scripts/release_info.sh
+++ dbsync-central/release_stuff/scripts/release_info.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # EIP RELEASES INFO
 #
-export RELEASE_NAME="1.0.0"
-export RELEASE_DATE="2022-01-08 18:00:00"
-export RELEASE_DESC="Initial Release"
+export RELEASE_NAME="EIP release 3.0.0.0"
+export RELEASE_DATE="2022-09-28 16:10:00"
+export RELEASE_DESC="New release with several improvments and new feactures"
 
-export OPENMRS_EIP_APP_RELEASE_URL="https://github.com/FriendsInGlobalHealth/openmrs-eip-docker/releases/download/v2.0.1.0/openmrs-eip-app-1.0-SNAPSHOT.jar"
+export OPENMRS_EIP_APP_RELEASE_URL="https://github.com/FriendsInGlobalHealth/openmrs-eip-docker/releases/download/v3.0.0.0/openmrs-eip-app-1.2-SNAPSHOT.jar"
