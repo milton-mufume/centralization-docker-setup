f642bad99ad9c62017c40bf61e7477a96d19c4b0
JPboane <jpboane@gmail.com>
2023-01-11 10:18:36 +0200

    Updated dbsync release to 1.1

--- dbsync-central/release_stuff/scripts/release_info.sh
+++ dbsync-central/release_stuff/scripts/release_info.sh
@@ -5,4 +5,4 @@ export RELEASE_NAME="EIP release 3.0.0.0"
 export RELEASE_DATE="2022-12-05 9:00:00"
 export RELEASE_DESC="New release with several improvments and new feactures"
 
-export OPENMRS_EIP_APP_RELEASE_URL="https://github.com/FriendsInGlobalHealth/openmrs-eip-docker/releases/download/v3.0.1.0/openmrs-eip-app-1.0.jar"
+export OPENMRS_EIP_APP_RELEASE_URL="https://github.com/FriendsInGlobalHealth/openmrs-eip-docker/releases/download/v3.0.1.0/openmrs-eip-app-1.1.jar"
