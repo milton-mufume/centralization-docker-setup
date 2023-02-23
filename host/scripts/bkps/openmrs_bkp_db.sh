7db04c73e81e196dfa2d99b564685cbb56e7b253
milton-mufume <fernando.mufume@fgh.org.mz>
2023-02-23 11:18:27 +0200

    Changing the project in order to have de backup files in host directory and to remake the backup scripts based on environment variables and specific backup for each database type

---
+++ host/scripts/bkps/openmrs_bkp_db.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# specific script for backup the openmrs database
+#
+source /home/mmufume/db/scripts/bkp_db.sh
+
+#
+docker exec $OPENMRS_DB_CONTAINER bash -c "/usr/bin/mysqldump -u $OPENMRS_DB_USER --password=$OPENMRS_DB_PASSWORD $OPENMRS_DB_NAME 2> /dev/null | gzip > /$OPENMRS_HOME_DIR_CONTAINER/${OPENMRS_DB_NAME}_db_$timestamp.sql.gz"
+#
+docker cp $OPENMRS_DB_CONTAINER:/$OPENMRS_HOME_DIR_CONTAINER/"${OPENMRS_DB_NAME}"_db_$timestamp.sql.gz $BKPS_HOME
+#
+docker exec $OPENMRS_DB_CONTAINER bash -c "rm /$OPENMRS_HOME_DIR_CONTAINER/${OPENMRS_DB_NAME}_db_$timestamp.sql.gz"
+#
+echo "BACKUP FINISHED" | tee  -a $LOG_DIR/bkps.log
