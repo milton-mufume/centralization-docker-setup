320acd9aa63fdd58b1cb3060b7660f80fa91ef28
JPboane <jpboane@gmail.com>
2023-09-11 09:29:33 +0200

    Start dbsync 1.4x releasing preparation

--- dbsync-central/release_stuff/scripts/release_info.sh
+++ dbsync-central/release_stuff/scripts/release_info.sh
@@ -3,6 +3,6 @@
 #
 export RELEASE_NAME="EIP-Release-5.0.0"
 export RELEASE_DATE="2023-06-20 12:00:00"
-export RELEASE_DESC="New release with several improvments and new feactures"
+export RELEASE_DESC="Release for dbsync 1.4x"
 
-export OPENMRS_EIP_APP_RELEASE_URL="https://github.com/csaude/openmrs-eip-docker/releases/download/V5.0.0/openmrs-eip-app-1.3.0.jar"
+export OPENMRS_EIP_APP_RELEASE_URL="https://github.com/csaude/openmrs-eip-docker/releases/download/V6.0.0/openmrs-eip-app-1.4.0.jar"
