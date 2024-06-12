1e863afe37386413aadd80bf1414fa4b55ba65e6
Daniel Chirinda <chirindzadaniel@gmail.com>
2024-06-12 08:33:38 +0200

    Update configurations for June release

--- dbsync-central/release_stuff/scripts/release_info.sh
+++ dbsync-central/release_stuff/scripts/release_info.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # EIP RELEASES INFO
 #
-export RELEASE_NAME="EIP-Release-8.0.0"
-export RELEASE_DATE="2024-03-20 20:00:00"
-export RELEASE_DESC="DBSYnc v1.6x Upgrade of Java version"
+export RELEASE_NAME="EIP-Release-9.0.0"
+export RELEASE_DATE="2024-06-20 20:00:00"
+export RELEASE_DESC="DBSync v1.7x, upgrade of OpenMRS to a new release and accommodation with DBSync"
 
-export OPENMRS_EIP_APP_RELEASE_URL="https://github.com/csaude/openmrs-eip-docker/releases/download/V8.0.0/openmrs-eip-app-1.6.0.jar"
-export CENTRALIZATION_FEATURES_MANAGER_RELEASE_URL="https://github.com/csaude/openmrs-eip-docker/releases/download/V8.0.0/centralization-features-manager-1.0.jar"
+export OPENMRS_EIP_APP_RELEASE_URL="https://github.com/csaude/openmrs-eip-docker/releases/download/V9.0.0/openmrs-eip-app-1.7.0.jar"
+export CENTRALIZATION_FEATURES_MANAGER_RELEASE_URL="https://github.com/csaude/openmrs-eip-docker/releases/download/V9.0.0/centralization-features-manager-1.0.jar"
